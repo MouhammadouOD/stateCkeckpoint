68e998c71bf5484dd8fb9cf17cf3c990a1bc8764
Mouhammadou Oury Diallo <ryhow99@gmail.com>
2021-08-21 15:00:32 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import range from './range.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    shows : true,
+    Person:{ fullName:'Range Rover',bio:'It s a very expensive car', imgSrc: range, profession:'run for fun'},
+    
+  };
+
+  
+  toggle = ()=>{(this.state.shows===true)? this.setState({shows:false}):this.setState({shows:true});}
+
+  render(){
+
+    
+        if (this.state.shows=== true) {
+          return(
+          <>
+            <h2>FullName : {this.state.Person.fullName}</h2>
+            <h2>Bio : {this.state.Person.bio}</h2>
+            <h2>Profession : {this.state.Person.profession}</h2><br/>
+            <button onClick={this.toggle}>click Me</button>
+            <img src={this.state.Person.imgSrc} alt='myprofile'/><br/><br/>
+          </>
+          )
+          
+        }
+        else{
+          return(
+          <>
+            <p>Person is hidden !!!!</p>
+            <br/>
+            <button onClick={this.toggle}>click Me</button>
+          </>
+          )
+          
+        }
+  }
 }
 
 export default App;
